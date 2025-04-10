11e8e4bede8b8b6c65e8f5500c73029c5ad63796
Timmy <t.cavanagh2k1@gmail.com>
2025-04-09 23:05:15 -0400

    Update

--- bot/command/impl/admin/adminblacklist.go
+++ bot/command/impl/admin/adminblacklist.go
@@ -7,7 +7,7 @@ import (
 	"github.com/TicketsBot/worker/bot/command/registry"
 	"github.com/TicketsBot/worker/bot/customisation"
 	"github.com/TicketsBot/worker/bot/dbclient"
-	"github.com/TicketsBot/worker/i18n"
+	"github.com/TicketsBot/worker/i18n" 
 	"github.com/rxdn/gdl/objects/interaction"
 	"strconv"
 	"time"
